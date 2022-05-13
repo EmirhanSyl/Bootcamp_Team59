07693befa1d324bddff606921c50168be588bf49
aysesirye <47401471+aysesirye@users.noreply.github.com>
2022-05-14 00:01:07 +0300

    desert map upgrade

---
+++ SourceCodeNA/Assets/Low Poly Castle Siege Pack/Components/Scripts/LightFlicker.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+	
+	public float minIntensity = 2.0f;
+	public float maxIntensity = 3.0f;
+	public Light light;
+
+	//Make the light flicker randomly
+	void Update () {
+		light.intensity = Random.Range (minIntensity, maxIntensity);
+	}
+}
