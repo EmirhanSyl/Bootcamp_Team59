1ce20940988afda629e48679c85c0f9d5c2eb762
Kafkass <ytpazarlikli@gmail.com>
2022-05-28 12:57:06 +0300

    LimitterForBuilds
    Build sisteminin canvasına ilk adım atıldı

--- SourceCodeNA/Assets/GameFolder/Scripts/BuildingSystem/BluePrintCanvas.cs
+++ SourceCodeNA/Assets/GameFolder/Scripts/BuildingSystem/BluePrintCanvas.cs
@@ -1,18 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BluePrintCanvas : MonoBehaviour
 {
     [SerializeField]GameObject[] _build;
 
-    public void InstantiateTheBuild0()
+    [SerializeField] TextMeshProUGUI priceText;
+
+    int _foodCost;
+    int _woodCost;
+    int _stoneCost;
+
+    //ne kadar bina yerlestirilecegini ayarlayacagiz
+    int _isCenterPlaced;
+    int _isHousesPlaced;
+
+    private void LateUpdate()
     {
-        Instantiate(_build[0]);
+        LimitterForBuildButtons();
     }
-    public void InstantiateTheBuild1()
+
+    void LimitterForBuildButtons()
     {
-        Instantiate(_build[1]);
+        if (_isCenterPlaced != 0)
+        {
+            this.transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+        if (_isHousesPlaced > 5)
+        {
+            this.transform.GetChild(1).gameObject.SetActive(false);
+        }
     }
-    
+
+    public void InstantiateTheColonyCenter()
+    {
+        _foodCost = 0;
+        _woodCost = 0;
+        _stoneCost = 0;
+
+        if (Storage._food >= _foodCost && Storage._wood >= _woodCost && Storage._stone >= _stoneCost)
+        {                        
+            Storage._food -= _foodCost;
+            Storage._wood -= _woodCost;
+            Storage._stone -= _stoneCost;
+            Instantiate(_build[0]);
+        }
+        _isCenterPlaced++;
+    }
+    public void InstantiateHouse()
+    {
+        _foodCost = 0;
+        _woodCost = 0;
+        _stoneCost = 0;
+
+        if (Storage._food >= _foodCost && Storage._wood >= _woodCost && Storage._stone >= _stoneCost)
+        {
+            Storage._food -= _foodCost;
+            Storage._wood -= _woodCost;
+            Storage._stone -= _stoneCost;
+            Instantiate(_build[1]);
+        }
+        _isHousesPlaced++;
+    }
+
+    public void InstantiateBarrack()
+    {
+        _foodCost = 0;
+        _woodCost = 0;
+        _stoneCost = 0;
+
+        if (Storage._food >= _foodCost && Storage._wood >= _woodCost && Storage._stone >= _stoneCost)
+        {
+            Storage._food -= _foodCost;
+            Storage._wood -= _woodCost;
+            Storage._stone -= _stoneCost;
+            Instantiate(_build[1]);
+        }
+    }
+
+    public void InstantiateMagician()
+    {
+        _foodCost = 0;
+        _woodCost = 0;
+        _stoneCost = 0;
+
+        if (Storage._food >= _foodCost && Storage._wood >= _woodCost && Storage._stone >= _stoneCost)
+        {
+            Storage._food -= _foodCost;
+            Storage._wood -= _woodCost;
+            Storage._stone -= _stoneCost;
+            Instantiate(_build[1]);
+        }
+    }
 }
