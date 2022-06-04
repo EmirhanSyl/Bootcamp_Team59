1ca1211a7b3a9d4fae6b2b83fba32a2627a2bb62
Kafkass <ytpazarlikli@gmail.com>
2022-06-04 04:51:18 +0300

    CollabrationMaps

--- SourceCodeNA/Assets/GameFolder/Scripts/BuildingSystem/BuildingScripts/OpenBuildingPanelScript.cs
+++ SourceCodeNA/Assets/GameFolder/Scripts/BuildingSystem/BuildingScripts/OpenBuildingPanelScript.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class OpenBuildingPanelScript : MonoBehaviour
 {
     GameObject _buildingCanvas;
+    GameObject _rtsCam;
+    GameObject _playerCam;
 
     private void Awake()
     {
         _buildingCanvas = GameObject.FindGameObjectWithTag("BuildingCanvas");
+        _rtsCam = GameObject.FindGameObjectWithTag("RTSCam");
+        _playerCam = GameObject.FindGameObjectWithTag("PlayerCam");
     }
 
     private void OnTriggerEnter(Collider other)
@@ -16,6 +20,8 @@ public class OpenBuildingPanelScript : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             _buildingCanvas.transform.GetChild(0).gameObject.SetActive(true);
+            _rtsCam.transform.GetChild(0).gameObject.SetActive(true);
+            _playerCam.transform.GetChild(0).gameObject.SetActive(false);
         }
     }
 
@@ -24,6 +30,8 @@ public class OpenBuildingPanelScript : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             _buildingCanvas.transform.GetChild(0).gameObject.SetActive(false);
+            _rtsCam.transform.GetChild(0).gameObject.SetActive(false);
+            _playerCam.transform.GetChild(0).gameObject.SetActive(true);
         }
     }
 }
--- SourceCodeNA/Assets/GameFolder/Scripts/CameraControllers/LookAtCam.cs
+++ SourceCodeNA/Assets/GameFolder/Scripts/CameraControllers/LookAtCam.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class LookAtCam : MonoBehaviour
 {
     Camera _cam;
+    GameObject _playerCam;
 
     private void Awake()
     {
         _cam = Camera.main;
+        _playerCam = GameObject.FindGameObjectWithTag("PlayerCam");
     }
 
     private void Update()
     {
         transform.eulerAngles = _cam.transform.eulerAngles;
+        transform.eulerAngles = _playerCam.transform.eulerAngles;
     }
 }
